ea3ee355e07ec625029e70caf09c7d0e8c97af24
Rajan Chaudhari <rajanchaudharigits@gmail.com>
2023-03-11 17:03:29 +0530

    Using React Hook: useEffect to execute side effects from within functional component and performing cleanup task to animate Cart Button when Meal Items are added in Cart.

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,19 +1,36 @@
-import { Fragment, useContext } from "react";
+import { Fragment, useContext, useState, useEffect } from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 import CartContext from "../../../CartContext/cart-context";
 
 const HeaderCartButton = (properties) => {
   const headerCartButtonContext = useContext(CartContext);
+  const [bumpCartButton, setBumpCartButton] = useState(false);
   const numberOfItemsInCart = headerCartButtonContext.items.reduce(
     (currentNumber, item) => {
       return currentNumber + item.amount;
     },
     0
   );
+  const styleCartButton = `${styles.button} ${
+    bumpCartButton ? styles.bump : ""
+  }`;
+  const { items } = headerCartButtonContext;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBumpCartButton(true);
+    const timeoutBump = setTimeout(() => {
+      setBumpCartButton(false);
+    }, 300);
+    return () => {
+      clearTimeout(timeoutBump);
+    };
+  }, [items]);
   return (
     <Fragment>
-      <button className={styles.button} onClick={properties.onClick}>
+      <button className={styleCartButton} onClick={properties.onClick}>
         <span className={styles.icon}>
           <CartIcon />
         </span>
